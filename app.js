a19bf9c2c5009b79c25bb77860b26c5fe1b3ee92
Otavio Ogo <otavioeidi@hotmail.com>
2022-06-29 21:24:36 -0300

    Criacao completo User

--- app.js
+++ app.js
@@ -7,9 +7,69 @@ const jwt = require('jsonwebtoken')
 
 const app = express()
 
+//COnfigurando JSON response
+app.use(express.json())
+
+
+//Models
+const User = require('./models/User')
+
 //Criando uma Rota Publica
 app.get('/', (req, res) => {
     res.status(200).json({msg: "Bem vindo a nossa API"})
+})
+
+
+//Registrar Usuario
+app.post('/auth/register', async(req, res) => {
+    const {name, email, password, confirmpassword} = req.body
+
+    //Validacoes
+    if (!name) {
+        return res.status(422).json({ msg: 'O nome e Obrigatorio!' })
+    }
+
+    if (!email) {
+        return res.status(422).json({ msg: 'O email e Obrigatorio!' })
+    }
+
+    if (!password) {
+        return res.status(422).json({ msg: 'A senha e Obrigatorio!' })
+    }
+
+    if(password !== confirmpassword) {
+        return res.status(422).json({ msg: 'As senhas nao conferem' })
+    }
+
+    //Verificar se o Usuario Existe
+    const userExists = await User.findOne({ email: email })
+        if(userExists) {
+            return res.status(422).json({ msg: 'Por favor Utilize Outro email!' })
+        }
+
+    //Criando Senha
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    //Criando User
+    const user = new User({
+        name, 
+        email,
+        password: passwordHash,
+    })
+
+    try {
+        await user.save()
+        res.status(201).json({msg: 'Usuario Criado com sucesso'})
+
+    }catch (error){
+
+        console.log(error)
+
+        res
+        .status(500)
+        .json({ msg: 'Error no Servidor'})
+    } 
 })
 
 //Credenciais
